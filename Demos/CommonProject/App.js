e7ad8e5f1e7be610da32403dafed47413b912a04
jianghongbing <jianghongbing1989@qq.com>
2019-03-01 16:03:22 +0800

    update flex

--- Demos/CommonProject/App.js
+++ Demos/CommonProject/App.js
@@ -33,8 +33,16 @@ export default class App extends Component<Props> {
           [1, 2, 3].map(number=><Item key={number} number={number}/>)
         }
         </View>
-        <View style={styles.greenView}></View>
-        <View style={styles.blueView}></View>
+        <View style={styles.greenView}>
+        {
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(number=><Item key={number} number={number} />)
+        }
+        </View>
+        <View style={styles.blueView}>
+        {
+          [1, 2, 3, 4, 5, 6, 7, 8, 9].map(number=><Item key={number} number={number} />)
+        }
+        </View>
       </View>
     );
   }
@@ -44,8 +52,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'flex-start',
-    alignItems: 'stretch',
-    alignContent: 'stretch',
+    // alignItems: 'stretch',
+    // alignContent: 'stretch',
     backgroundColor: '#F5FCFF',
   },
 
@@ -56,6 +64,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
     flexDirection: 'row',
     justifyContent: 'space-around',
+    alignItems: 'flex-end',
   },
 
   greenView: {
@@ -63,6 +72,9 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: 'blue',
     backgroundColor: 'green',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    flexWrap: 'wrap',
   },
 
   blueView: {
@@ -70,18 +82,23 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'red',
     backgroundColor: 'blue',
+    padding: 20,
+    flexDirection: 'row-reverse',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    alignContent: 'space-around',
   },
 
   item: {
     width: 50,
     height: 50,
     backgroundColor: 'orange',
+    justifyContent:'center',
   },
 
   itemText: {
     color: 'white',
     fontSize: 20,
     textAlign: 'center',
-    textAlignVertical: 'center',
   }
 });
